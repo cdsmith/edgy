452ed9bbeaa55d0b71a5b3092e52de64aae9ca67
Chris Smith <cdsmith@gmail.com>
2022-05-22 02:27:07 -0400

    API stuff

--- app/Main.hs
+++ app/Main.hs
@@ -1,41 +1,77 @@
-{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Main where
 
 import Control.Monad.ST (RealWorld)
 import Data.Unique.Tag (Tag, newTag)
 import GHC.IO.Exception (IOErrorType (PermissionDenied))
 import System.IO.Unsafe (unsafePerformIO)
+import Data.Kind (Type)
+import Control.Concurrent.STM (STM)
 
 ------ ACTUAL IMPL -------------
 
-class Schema g where
-    data Relation g a b
+-- Schema API:
 
-data Relation2 a b = Relation2 (Tag RealWorld (a, b)) deriving (Eq, Ord)
+data RelTuple (a :: k) (b :: k)
+data FieldTuple (a :: k) (b :: Type)
+
+data Relation (a :: k) (b :: k) = Relation (Tag RealWorld (RelTuple a b)) deriving (Eq, Ord)
+
+data Field (a :: k) (b :: Type) = Field (Tag RealWorld (FieldTuple a b)) deriving (Eq, Ord)
+
+newRelation :: forall k (a :: k) (b :: k). IO (Relation a b)
+newRelation = Relation <$> newTag
+
+newField :: forall k (a :: k) (b :: Type). IO (Field a b)
+newField = Field <$> newTag
+
+data Node (a :: k)
+
+-- Low-level API:
+
+newNode :: STM (Node a)
+newNode = undefined
+
+setField :: Node a -> Field a t -> t -> STM ()
+setField = undefined
+
+getField :: Node a -> Field a t -> STM t
+getField = undefined
+
+addToRelation :: Node a -> Relation a b -> Node b -> STM ()
+addToRelation = undefined
+
+removeFromRelation :: Node a -> Relation a b -> Node b -> STM ()
+removeFromRelation = undefined
+
+getRelated :: Node a -> Relation a b -> STM [Node b]
+getRelated = undefined
+
+-- Query API: TBD
 
 ------ EXAMPLE -----------------
-data PeopleAndHobbies
 
-data Person = Person {personName :: String}
+data Person
 
-data Activity = Activity {activityName :: String}
+data Activity
 
-instance Schema PeopleAndHobbies where
-    data Relation PeopleAndHobbies a b where
-        Friend :: Relation PeopleAndHobbies Person Person
-        Hobby :: Relation PeopleAndHobbies Person Activity
-        Child :: Relation PeopleAndHobbies Person Person
-        Spouse :: Relation PeopleAndHobbies Person Person
+personName :: Field Person String
+personName = unsafePerformIO newField
 
-data Shae'sSchema
+activityName :: Field Activity String
+activityName = unsafePerformIO newField
 
-instance Schema Shae'sSchema where
-    data Relation Shae'sSchema a b where
-        ShaeHobby :: Relation Shae'sSchema Person Activity
+friend :: Relation Person Person
+friend = unsafePerformIO newRelation
 
-friend :: Relation2 Person Person
-friend = Relation2 (unsafePerformIO newTag)
+hobby :: Relation Person Person
+hobby = unsafePerformIO newRelation
+
+----------------------------------------
 
 main :: IO ()
 main = return ()
