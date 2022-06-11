95f1ff952138f96277fa292565be79c3dd481c2f
Chris Smith <cdsmith@gmail.com>
2022-06-11 17:00:25 -0400

    Remove bad semigroup instance for Cardinality

--- app/Cardinality.hs
+++ app/Cardinality.hs
@@ -18,18 +18,6 @@ import Data.Typeable (Typeable)
 
 data Cardinality = Optional | One | Many | Some
 
-instance Semigroup Cardinality where
-  One <> c = c
-  c <> One = c
-  Optional <> Optional = Optional
-  Optional <> _ = Many
-  _ <> Optional = Many
-  Some <> Some = Some
-  _ <> _ = Many
-
-instance Monoid Cardinality where
-  mempty = One
-
 type Numerous :: Cardinality -> Type -> Type
 type family Numerous c t where
   Numerous Optional t = Maybe t
