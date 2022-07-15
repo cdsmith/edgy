d86672dd29ef89dda94dc2be5d6426e9fff16d9b
Chris Smith <cdsmith@gmail.com>
2022-07-15 06:35:47 -0400

    Remove TypeRep from DB keys

--- src/Edgy/DB.hs
+++ src/Edgy/DB.hs
@@ -12,7 +12,6 @@ module Edgy.DB
     closeDB,
     DBRef,
     DBStorable (..),
-    tryGetDBRef,
     getDBRef,
     readDBRef,
     writeDBRef,
@@ -22,7 +21,7 @@ module Edgy.DB
   )
 where
 
-import Control.Concurrent (forkIO, newEmptyMVar, MVar, putMVar, takeMVar)
+import Control.Concurrent (MVar, forkIO, newEmptyMVar, putMVar, takeMVar)
 import Control.Concurrent.STM
   ( STM,
     TVar,
@@ -35,32 +34,21 @@ import Control.Concurrent.STM
   )
 import Control.Monad (forM_, when)
 import Control.Monad.Extra (whileM)
-import Data.Binary (Binary, decode, encode)
+import Data.Binary (decode, encode)
 import Data.Bool (bool)
-import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString as BS
-import Data.Hashable (Hashable)
+import Data.ByteString.Lazy (ByteString)
 import Data.Map (Map)
 import qualified Data.Map.Strict as Map
 import Data.Proxy (Proxy (..))
-import Data.Typeable (Typeable, typeRep, typeRepFingerprint)
+import Data.Typeable (TypeRep, Typeable, typeRep)
 import qualified Focus
 import GHC.Conc (unsafeIOToSTM)
-import GHC.Generics (Generic)
 import qualified StmContainers.Map as SMap
-import System.Directory (doesFileExist, removeFile, createDirectoryIfMissing)
+import System.Directory (createDirectoryIfMissing, doesFileExist, removeFile)
 import System.FilePath ((</>))
 import System.Mem.Weak (Weak, addFinalizer, deRefWeak, mkWeak)
 import Unsafe.Coerce (unsafeCoerce)
-import GHC.Fingerprint (Fingerprint)
-
-data DBKey = DBKey Fingerprint String deriving (Eq, Ord, Show, Generic)
-
-instance Hashable DBKey
-instance Binary DBKey
-
-keyToString :: DBKey -> String
-keyToString (DBKey tyfp s) = show tyfp ++ "-" ++ s
 
 class Typeable a => DBStorable a where
   getDB :: DB -> ByteString -> STM a
@@ -69,19 +57,19 @@ class Typeable a => DBStorable a where
 data SomeTVar = forall a. DBStorable a => SomeTVar (TVar (Maybe a))
 
 data DBPersister = DBPersister
-  { dbReader :: DBKey -> IO (Maybe ByteString),
-    dbWriter :: Map DBKey (Maybe ByteString) -> IO ()
+  { dbReader :: String -> IO (Maybe ByteString),
+    dbWriter :: Map String (Maybe ByteString) -> IO ()
   }
 
 data DB = DB
-  { dbRefs :: SMap.Map DBKey (Weak SomeTVar),
-    dbDirty :: TVar (Map DBKey (SomeTVar, Maybe ByteString)),
+  { dbRefs :: SMap.Map String (TypeRep, Weak SomeTVar),
+    dbDirty :: TVar (Map String (SomeTVar, Maybe ByteString)),
     dbPersister :: DBPersister,
     dbClosing :: TVar Bool,
     dbClosed :: TVar Bool
   }
 
-data DBRef a = DBRef DB DBKey (TVar (Maybe a))
+data DBRef a = DBRef DB String (TVar (Maybe a))
 
 instance Eq (DBRef a) where
   DBRef _ k1 _ == DBRef _ k2 _ = k1 == k2
@@ -90,26 +78,27 @@ instance Ord (DBRef a) where
   compare (DBRef _ k1 _) (DBRef _ k2 _) = compare k1 k2
 
 instance Show (DBRef a) where
-  show (DBRef _ (DBKey _ s) _) = s
+  show (DBRef _ s _) = s
 
 instance DBStorable a => DBStorable (DBRef a) where
-  getDB db bs = getDBRef' db (decode bs)
+  getDB db bs = getDBRef db (decode bs)
   putDB (DBRef _ dbkey _) = encode dbkey
 
 filePersister :: FilePath -> IO DBPersister
 filePersister dir = do
   createDirectoryIfMissing True dir
-  return $ DBPersister
-    { dbReader = \key -> do
-        ex <- doesFileExist (dir </> keyToString key)
-        if ex
-          then Just <$> BS.fromStrict <$> BS.readFile (dir </> keyToString key)
-          else return Nothing,
-      dbWriter = \m -> forM_ (Map.toList m) $
-        \(key, mbs) -> case mbs of
-          Just bs -> BS.writeFile (dir </> keyToString key) (BS.toStrict bs)
-          Nothing -> removeFile (dir </> keyToString key)
-    }
+  return $
+    DBPersister
+      { dbReader = \key -> do
+          ex <- doesFileExist (dir </> key)
+          if ex
+            then Just <$> BS.fromStrict <$> BS.readFile (dir </> key)
+            else return Nothing,
+        dbWriter = \m -> forM_ (Map.toList m) $
+          \(key, mbs) -> case mbs of
+            Just bs -> BS.writeFile (dir </> key) (BS.toStrict bs)
+            Nothing -> removeFile (dir </> key)
+      }
 
 openDB :: DBPersister -> IO DB
 openDB persister = do
@@ -143,41 +132,30 @@ closeDB db = do
   atomically $ writeTVar (dbClosing db) True
   atomically $ readTVar (dbClosed db) >>= bool retry (return ())
 
-readForDB :: DBStorable a => DB -> DBKey -> MVar (Maybe a) -> IO ()
-readForDB db dbkey mvar = do
-  readResult <- dbReader (dbPersister db) dbkey
+readForDB :: DBStorable a => DB -> String -> MVar (Maybe a) -> IO ()
+readForDB db key mvar = do
+  readResult <- dbReader (dbPersister db) key
   case readResult of
     Just bs -> do
       v <- atomically (getDB db bs)
       putMVar mvar (Just v)
     Nothing -> putMVar mvar Nothing
 
-tryGetDBRef :: forall a. Typeable a => DB -> String -> STM (Maybe (DBRef a))
-tryGetDBRef db key = tryGetDBRef' db dbkey
-  where
-    dbkey = DBKey (typeRepFingerprint (typeRep (Proxy @a))) key
-
-tryGetDBRef' :: forall a. Typeable a => DB -> DBKey -> STM (Maybe (DBRef a))
-tryGetDBRef' db dbkey =
-  SMap.lookup dbkey (dbRefs db) >>= \case
-    Just weakRef ->
-      unsafeIOToSTM (deRefWeak weakRef) >>= \case
-        Just (SomeTVar ref) -> return (Just (DBRef db dbkey (unsafeCoerce ref)))
-        Nothing -> return Nothing
-    Nothing -> return Nothing
-
 getDBRef :: forall a. DBStorable a => DB -> String -> STM (DBRef a)
-getDBRef db key = getDBRef' db dbkey
-  where
-    dbkey = DBKey (typeRepFingerprint (typeRep (Proxy @a))) key
-
-getDBRef' :: forall a. DBStorable a => DB -> DBKey -> STM (DBRef a)
-getDBRef' db dbkey = maybe insert return =<< tryGetDBRef' db dbkey
+getDBRef db key =
+  SMap.lookup key (dbRefs db) >>= \case
+    Just (tr, weakRef)
+      | tr == typeRep (Proxy @a) ->
+          unsafeIOToSTM (deRefWeak weakRef) >>= \case
+            Just (SomeTVar ref) -> return (DBRef db key (unsafeCoerce ref))
+            Nothing -> insert
+      | otherwise -> error "Type mismatch in DBRef"
+    Nothing -> insert
   where
     insert = do
       v <- unsafeIOToSTM $ do
         mvar <- newEmptyMVar
-        _ <- forkIO $ readForDB db dbkey mvar
+        _ <- forkIO $ readForDB db key mvar
         takeMVar mvar
       ref <- newTVar v
       ptr <- unsafeIOToSTM $ do
@@ -186,17 +164,17 @@ getDBRef' db dbkey = maybe insert return =<< tryGetDBRef' db dbkey
           atomically $
             SMap.focus
               ( Focus.updateM
-                  ( \p ->
+                  ( \(tr, p) ->
                       unsafeIOToSTM (deRefWeak p) >>= \case
                         Nothing -> return Nothing
-                        Just _ -> return (Just p)
+                        Just _ -> return (Just (tr, p))
                   )
               )
-              dbkey
+              key
               (dbRefs db)
         return ptr
-      SMap.insert ptr dbkey (dbRefs db)
-      return (DBRef db dbkey ref)
+      SMap.insert (typeRep (Proxy @a), ptr) key (dbRefs db)
+      return (DBRef db key ref)
 
 readDBRef :: DBRef a -> STM (Maybe a)
 readDBRef (DBRef _ _ ref) = do
